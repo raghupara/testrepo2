0adc8528809938949b82876a685706dd9bbf1005
raghupara <44479450+raghupara@users.noreply.github.com>
2018-10-31 15:30:07 -0400

    Add files via upload

---
+++ demo/src/main/java/com/papajohns/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.papajohns.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
+
+/*public class DemoApplication {
+
+public static void main(String[] args) {
+	SpringApplication.run(DemoApplication.class, args);
+}
+}*/
